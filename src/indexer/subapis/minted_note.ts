3eeff27f143771bb96c16c86e1774ce3553129e1
Songkeys <songv587@gmail.com>
2022-07-14 19:58:03 +0800

    feat(indexer): add `getMintedNotesOfAddress` & `getMintedNotesOfNote`

--- src/indexer/subapis/minted_note.ts
+++ src/indexer/subapis/minted_note.ts
@@ -8,19 +8,56 @@ export class MintedNoteIndexer extends BaseIndexer {
    * This returns a list of minted notes.
    *
    * @category MintedNote
+   * @param address - The owner of the minted notes.
    * @param options - The options to send to the indexer.
    * @returns The list of minted notes.
    */
-  async getMintedNotes({
-    limit,
-    cursor,
-  }: {
-    /** Limit the count of items returned. */
-    limit?: number
-    /** Used for pagination. */
-    cursor?: string
-  } = {}): Promise<ListResponse<MintedNoteEntity>> {
-    let url = `${this.endpoint}/minted/notes?`
+  async getMintedNotesOfAddress(
+    address: string,
+    {
+      limit,
+      cursor,
+    }: {
+      /** Limit the count of items returned. */
+      limit?: number
+      /** Used for pagination. */
+      cursor?: string
+    } = {},
+  ): Promise<ListResponse<MintedNoteEntity>> {
+    let url = `${this.endpoint}/addresses/${address}/minted/notes?`
+    url += queryString.stringify({
+      limit,
+      cursor,
+    })
+
+    const res = await fetch(url).then((res) => res.json())
+
+    return res as ListResponse<MintedNoteEntity>
+  }
+
+  /**
+   * This returns a list of minted notes.
+   *
+   * @category MintedNote
+   * @param characterId - The characterId of the note minted.
+   * @param noteId - The noteId of the note minted.
+   * @param options - The options to send to the indexer.
+   * @returns The list of minted notes.
+   */
+  async getMintedNotesOfNote(
+    characterId: BigNumberish,
+    noteId: BigNumberish,
+    {
+      limit,
+      cursor,
+    }: {
+      /** Limit the count of items returned. */
+      limit?: number
+      /** Used for pagination. */
+      cursor?: string
+    } = {},
+  ): Promise<ListResponse<MintedNoteEntity>> {
+    let url = `${this.endpoint}/notes/${characterId}/${noteId}/minted?`
     url += queryString.stringify({
       limit,
       cursor,
