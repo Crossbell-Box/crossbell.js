e7d04eff5c8bda4dc8cf2750f7cc5cfce446e91b
Songkeys <songv587@gmail.com>
2022-09-20 17:30:46 +0800

    feat(types): export `CbtAbi`

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import { Indexer } from './indexer'
 import { Ipfs } from './ipfs'
 import type { Abi as EntryAbi } from './contract/abis/entry/types'
 import type { Abi as PeripheryAbi } from './contract/abis/periphery/types'
+import type { Abi as CbtAbi } from './contract/abis/cbt/types'
 
 export default { Network, Contract, Indexer, Ipfs }
-export { Network, Contract, Indexer, Ipfs, EntryAbi, PeripheryAbi }
+export { Network, Contract, Indexer, Ipfs, EntryAbi, PeripheryAbi, CbtAbi }
 export * from './types'
 export * as utils from './utils'
 
