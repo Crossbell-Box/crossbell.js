772b4bb8166439e28ce493150dc09906199b7908
Songkeys <songv587@gmail.com>
2022-09-23 14:29:43 +0800

    feat(indexer): add `status` query to indexer.getAchievement

--- src/indexer/subapis/achievement.ts
+++ src/indexer/subapis/achievement.ts
@@ -12,15 +12,16 @@ export class AchievementIndexer extends BaseIndexer {
    * This returns a list of achievements owned by a specific address.
    * @category Achievement
    * @param characterId - The id of the character.
+   * @param options - The options to send to the indexer.
    * @returns The list of achievements.
    */
   async getAchievement(
     characterId: BigNumberish,
-    { minted }: { minted?: boolean } = {},
+    { status }: { status?: AchievementItem['status'][] } = {},
   ): Promise<ListResponse<AchievementSection> | null> {
     let url = `${this.endpoint}/characters/${characterId}/achievements?`
 
-    url + queryString.stringify({ minted })
+    url += queryString.stringify({ status })
 
     const res = await fetch(url).then((res) => res.json())
 
