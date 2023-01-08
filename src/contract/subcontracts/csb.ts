71a77f85cbd2cad70263847007e7b281f7d9c877
Songkeys <songv587@gmail.com>
2023-01-08 20:56:21 +0800

    feat(contract): add `contract.transferCsb()` method

--- src/contract/subcontracts/csb.ts
+++ src/contract/subcontracts/csb.ts
@@ -1,6 +1,7 @@
 import { BaseContract } from './base'
 import { autoSwitchMainnet } from '../decorators'
 import type { Result } from '../../types/contract'
+import type { BigNumberish, providers } from 'ethers'
 
 export class CsbContract extends BaseContract {
   /**
@@ -15,6 +16,34 @@ export class CsbContract extends BaseContract {
     const balance = await this.contract.provider.getBalance(owner)
     return {
       data: balance.toString(),
+    }
+  }
+
+  /**
+   * It sends $CSB to the address.
+   * @category CSB
+   * @param {string} toAddress - The address to send $CSB to.
+   * @param {string} amount - The amount of $CSB to send. (in wei)
+   * @returns The $CSB balance of the owner.
+   */
+  @autoSwitchMainnet()
+  async transferCsb(
+    toAddress: string,
+    amount: BigNumberish,
+  ): Promise<Result<{}, true>> | never {
+    this.validateAddress(toAddress)
+    // https://github.com/wagmi-dev/wagmi/blob/3e9145bdfc311f6eaeffe0747d03455f548d918c/packages/core/src/actions/transactions/sendTransaction.ts#L82-L100
+    const signer = this.contract.signer
+    const uncheckedSigner =
+      (signer as providers.JsonRpcSigner).connectUnchecked?.() ?? signer
+    const tx = await uncheckedSigner.sendTransaction({
+      to: toAddress,
+      value: amount,
+    })
+    const receipt = await tx.wait()
+    return {
+      data: {},
+      transactionHash: receipt.transactionHash,
     }
   }
 }
