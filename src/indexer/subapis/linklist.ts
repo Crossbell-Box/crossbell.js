9d99351afe788ec184eb4a5b1cf9fdec74fb7fe3
Songkeys <songv587@gmail.com>
2023-03-15 12:19:31 +0800

    feat!(indexer): Siince linklists is owned by character and not address now, this version removed `indexer.getLinklistsByAddress()`; renamed `indexer.getLinklistsByCharacter` -> `indexer.getLinklistsOfCharacter()`

--- src/indexer/subapis/linklist.ts
+++ src/indexer/subapis/linklist.ts
@@ -5,55 +5,19 @@ import { createSearchParamsString } from '../../utils/query_string'
 
 export class LinklistIndexer extends BaseIndexer {
   /**
-   * This returns a list of linklists owned by a specific address.
-   * @category Linklist
-   * @param address - The address of the linklists owner.
-   * @param options - The options to send to the indexer.
-   * @returns The list of linklist.
-   */
-  async getLinklistsByAddress(
-    address: string,
-    {
-      attached = false,
-      linkType,
-      limit = 20,
-      cursor,
-    }: {
-      /** If true, return only the attached linklists. */
-      attached?: boolean
-      /** The link type to filter by. */
-      linkType?: string
-      /** Limit the count of items returned. */
-      limit?: number
-      /** Used for pagination. */
-      cursor?: string
-    } = {},
-  ): Promise<ListResponse<LinklistEntity>> {
-    let url = `${this.endpoint}/addresses/${address}/linklists?`
-    url += createSearchParamsString({ attached, linkType, limit, cursor })
-
-    const res = await this.fetch(url).then((res) => res.json())
-
-    return res as ListResponse<LinklistEntity>
-  }
-
-  /**
-   * This returns a list of linklists attached to a specific character.
+   * This returns a list of linklists of a specific character.
    * @category Linklist
    * @param characterId - The characterId of the linklists owner.
    * @param options - The options to send to the indexer.
    * @returns The list of linklist.
    */
-  async getLinklistsByCharacter(
+  async getLinklistsOfCharacter(
     characterId: BigNumberish,
     {
-      attached = false,
       linkType,
       limit = 20,
       cursor,
     }: {
-      /** If true, return only the attached linklists. */
-      attached?: boolean
       /** The link type to filter by. */
       linkType?: string
       /** Limit the count of items returned. */
@@ -63,7 +27,7 @@ export class LinklistIndexer extends BaseIndexer {
     } = {},
   ): Promise<ListResponse<LinklistEntity>> {
     let url = `${this.endpoint}/characters/${characterId}/linklists?`
-    url += createSearchParamsString({ attached, linkType, limit, cursor })
+    url += createSearchParamsString({ linkType, limit, cursor })
 
     const res = await this.fetch(url).then((res) => res.json())
 
