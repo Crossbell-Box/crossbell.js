f2790edebb47236621d361ae163e1ec14c0d4100
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-04-15 03:27:50 +0800

    chore: fix lint

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
-export default defineConfig(async ({ mode }) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     resolve: {
