82b054ae9b483bdd3c77189255371d621f81f365
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-04-19 17:30:48 +0800

    feat(examples/vite): add indexer

--- examples/vite/src/App.vue
+++ examples/vite/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Contract } from 'crossbell.js'
+import { Contract, Indexer } from 'crossbell.js'
 import { onErrorCaptured, ref } from 'vue'
 import { useDark, useEventListener, useLocalStorage } from '@vueuse/core'
 import { type Address } from 'abitype'
@@ -14,18 +14,22 @@ onErrorCaptured((err) => {
 })
 
 const metamask = window.ethereum
-
 const contract = new Contract(metamask)
+
+const indexer = new Indexer()
+
 const address = useLocalStorage<Address>('address', '0x')
 const characterId = useLocalStorage('characterId', '')
 const handle = useLocalStorage('handle', '')
 const result = ref('')
+const loading = ref(false)
 
 async function connect() {
   await showResult(contract.walletClient!.requestAddresses())
 }
 
 async function showResult(p: any) {
+  loading.value = true
   try {
     result.value = JSON.stringify(
       await p,
@@ -35,6 +39,8 @@ async function showResult(p: any) {
     )
   } catch (err: any) {
     result.value = err
+  } finally {
+    loading.value = false
   }
 }
 
@@ -57,21 +63,33 @@ function getCharacterByHandle() {
 function getCharacter() {
   showResult(contract.character.get(BigInt(characterId.value)))
 }
+
 function setPrimaryCharacterId() {
   showResult(contract.character.setPrimaryId(+characterId.value))
+}
+
+function getCharacters() {
+  showResult(indexer.character.getMany(address.value))
+}
+
+function getNotes() {
+  showResult(indexer.note.getMany({ characterId: characterId.value }))
+}
+function getFeeds() {
+  showResult(indexer.feed.getManyByCharacter(characterId.value))
 }
 </script>
 
 <template>
-  <h1 text="3xl" py4>Crossbell.js Demo</h1>
+  <h1 text="3xl" py4 text-center font-mono>Crossbell.js Demo</h1>
   <hr />
-  <div flex="~ gap2 wrap" font-mono>
+  <div flex="~ gap2 wrap" font-mono mb2>
     <input v-model="address" type="text" placeholder="address" />
     <input v-model="characterId" type="text" placeholder="characterId" />
     <input v-model="handle" type="text" placeholder="handle" />
   </div>
-  <hr />
-  <div flex="~ gap2 wrap">
+  <fieldset flex="~ gap2 wrap" border p2 mb2>
+    <legend>Contract</legend>
     <button @click="connect">connect</button>
     <button @click="balance">balance</button>
     <button @click="transfer">transfer</button>
@@ -79,9 +97,14 @@ function setPrimaryCharacterId() {
     <button @click="getCharacter">getCharacter</button>
     <button @click="getCharacterByHandle">getCharacterByHandle</button>
     <button @click="setPrimaryCharacterId">setPrimaryCharacterId</button>
-  </div>
-  <hr />
-  <pre whitespace-pre-wrap break-words bg="gray/80" p4 rounded-4>{{
-    result
+  </fieldset>
+  <fieldset flex="~ gap2 wrap" border p2>
+    <legend>Indexer</legend>
+    <button @click="getCharacters">getCharacters</button>
+    <button @click="getNotes">getNotes</button>
+    <button @click="getFeeds">getFeeds</button>
+  </fieldset>
+  <pre whitespace-pre-wrap break-words bg="gray/80" p4 my4 rounded-4>{{
+    loading ? 'Loading...' : result
   }}</pre>
 </template>
