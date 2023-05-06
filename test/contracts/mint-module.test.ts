d179921c57be3da5899fba3892460acc16cb7321
Songkeys <songv587@gmail.com>
2023-05-07 04:16:56 +0800

    test: fix mint-module test (#36)

--- test/contracts/mint-module.test.ts
+++ test/contracts/mint-module.test.ts
@@ -1,5 +1,9 @@
 import { describe, expect, test } from 'vitest'
-import { decodeModuleInitData, encodeModuleInitData } from '../../src'
+import {
+  decodeModuleInitData,
+  encodeModuleInitData,
+  getModules,
+} from '../../src'
 
 describe('mind-module', () => {
   const input = [
@@ -11,19 +15,16 @@ describe('mind-module', () => {
   ]
   const output =
     '0x0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020000000000000000000000001234567890123456789012345678901234567890000000000000000000000000abc123def456abc123def456abc123def456abc1'
+
   test('encodeModuleInitData', async () => {
-    const result = await encodeModuleInitData(
-      '0x328610484ba1faae0fcdee44990d199cd84c8608',
-      input,
-    )
+    const modules = await getModules()
+    const result = await encodeModuleInitData(modules[0].address, input)
     expect(result).toBe(output)
   })
 
   test('decodeModuleInitData', async () => {
-    const result = await decodeModuleInitData(
-      '0x328610484ba1faae0fcdee44990d199cd84c8608',
-      output,
-    )
+    const modules = await getModules()
+    const result = await decodeModuleInitData(modules[0].address, output)
     expect(result).toStrictEqual(input)
   })
 })
