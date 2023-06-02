7cd0bf0c82dac909ce8b193881439b662f6a0272
Runjuu <hi@runjuu.com>
2023-06-01 19:25:18 -0700

    feat(utils/validate-handle): simplify return type

--- src/utils/validate-handle.ts
+++ src/utils/validate-handle.ts
@@ -1,46 +1,24 @@
 import type { Indexer } from '../indexer'
 
-export type ValidateHandleErrorKind =
-  | 'existed'
-  | 'lengthInvalid'
-  | 'charsInvalid'
-
-export type ValidateHandleResult =
-  | { isValid: true; error?: undefined; errorMsg?: undefined }
-  | { isValid: false; error: ValidateHandleErrorKind; errorMsg: string }
-
-const ERROR_MSG_MAP: Record<ValidateHandleErrorKind, string> = {
-  existed: 'The handle is already in use.',
-  lengthInvalid: 'The handle must be between 3 and 31 characters.',
-  charsInvalid:
-    'The handle must only contain lower-case letters, numbers, hyphens (-), or underscores (_).',
-}
+export type ValidateHandleError = 'existed' | 'lengthInvalid' | 'charsInvalid'
 
 export async function validateHandle(
   handle: string,
   indexer: Indexer,
-): Promise<ValidateHandleResult> {
+): Promise<ValidateHandleError | null> {
   if (handle.length >= 32 || handle.length <= 2) {
-    return invalid('lengthInvalid')
+    return 'lengthInvalid'
   }
 
   if (!/^[\d_a-z-]+$/.test(handle)) {
-    return invalid('charsInvalid')
+    return 'charsInvalid'
   }
 
   if (await checkIfExisted(handle, indexer)) {
-    return invalid('existed')
+    return 'existed'
   }
 
-  return { isValid: true }
-}
-
-function invalid(kind: ValidateHandleErrorKind): ValidateHandleResult {
-  return {
-    isValid: false,
-    error: kind,
-    errorMsg: ERROR_MSG_MAP[kind],
-  }
+  return null
 }
 
 async function checkIfExisted(
