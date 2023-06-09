52e12a1850e862aca89d33be4138ee59c5e6c72f
Runjuu <hi@runjuu.com>
2023-06-09 10:28:58 -0700

    chore(utils/validate-handle): add comments

--- src/utils/validate-handle.ts
+++ src/utils/validate-handle.ts
@@ -1,7 +1,24 @@
 import { type Indexer } from '../indexer'
 
+/**
+ * The ValidateHandleError type represents the type of validation errors that can occur.
+ *
+ * - 'existed': The handle is already in use.
+ * - 'lengthInvalid': The handle must be between 3 and 31 characters.
+ * - 'charsInvalid': The handle must only contain lower-case letters, numbers, hyphens (-), or underscores (_).
+ */
 export type ValidateHandleError = 'existed' | 'lengthInvalid' | 'charsInvalid'
 
+/**
+ * Validate a handle.
+ *
+ * If no Indexer is provided, it will only check the format of the handle.
+ * If an Indexer is provided, it will also check if the handle is already in use.
+ *
+ * @param handle - The handle to be validated.
+ * @param indexer - An optional Indexer for checking the existence of the handle.
+ * @returns A Promise that resolves to a ValidateHandleError type error indicator, or null if the handle is valid.
+ */
 export async function validateHandle(
   handle: string,
   indexer?: Indexer,
