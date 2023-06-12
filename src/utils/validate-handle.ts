734f4661cb6837415cc8b1293c830d476272ccf4
Runjuu <hi@runjuu.com>
2023-06-11 21:15:00 -0700

    feat(utils/validate-handle): mark `indexer` as required
    https://github.com/Crossbell-Box/crossbell.js/pull/42#discussion_r1223848616

--- src/utils/validate-handle.ts
+++ src/utils/validate-handle.ts
@@ -12,16 +12,13 @@ export type ValidateHandleError = 'existed' | 'lengthInvalid' | 'charsInvalid'
 /**
  * Validate a handle.
  *
- * If no Indexer is provided, it will only check the format of the handle.
- * If an Indexer is provided, it will also check if the handle is already in use.
- *
  * @param handle - The handle to be validated.
- * @param indexer - An optional Indexer for checking the existence of the handle.
+ * @param indexer - Indexer for checking the existence of the handle.
  * @returns A Promise that resolves to a ValidateHandleError type error indicator, or null if the handle is valid.
  */
 export async function validateHandle(
   handle: string,
-  indexer?: Indexer,
+  indexer: Indexer,
 ): Promise<ValidateHandleError | null> {
   if (handle.length >= 32 || handle.length <= 2) {
     return 'lengthInvalid'
@@ -31,7 +28,7 @@ export async function validateHandle(
     return 'charsInvalid'
   }
 
-  if (indexer && (await checkIfExisted(handle, indexer))) {
+  if (await checkIfExisted(handle, indexer)) {
     return 'existed'
   }
 
