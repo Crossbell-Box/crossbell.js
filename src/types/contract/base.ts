87ab6fe318d7f7ffe8c2c84879f7565b34480121
Songkeys <songv587@gmail.com>
2023-07-06 14:07:57 +0800

    chore: bump deps

--- src/types/contract/base.ts
+++ src/types/contract/base.ts
@@ -14,17 +14,25 @@ export type Result<T, HasTxHash extends boolean = false> = {
   data: T
 } & (HasTxHash extends true ? { transactionHash: Address } : {})
 
+/**
+ * @description Construct a type with the properties of union type T except for those in type K.
+ * @example
+ * type Result = UnionOmit<{ a: string, b: number } | { a: string, b: undefined, c: number }, 'a'>
+ * => { b: number } | { b: undefined, c: number }
+ */
+type UnionOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never
+
 export type ReadOverrides<
   TAbi extends Abi,
   TFunctionName extends ExtractAbiFunctionNames<TAbi, 'pure' | 'view'>,
-> = Omit<
+> = UnionOmit<
   ReadContractParameters<TAbi, TFunctionName>,
   'abi' | 'address' | 'args' | 'functionName'
 >
 export type WriteOverrides<
   TAbi extends Abi,
   TFunctionName extends ExtractAbiFunctionNames<TAbi, 'nonpayable' | 'payable'>,
-> = Omit<
+> = UnionOmit<
   WriteContractParameters<TAbi, TFunctionName, Chain, Account>,
   'abi' | 'address' | 'args' | 'functionName'
 >
